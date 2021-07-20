1409deb7d9a57a80333e9660b90f62a371476b38
zhouyu <zhouyu-jt@163.com>
2021-07-20 15:19:34 +0800

    完善 daxia::String

--- string.hpp
+++ string.hpp
@@ -1,6 +1,6 @@
 /*!
 * Licensed under the MIT License.See License for details.
-* Copyright (c) 2018 漓江里的大虾.
+* Copyright (c) 2021 漓江里的大虾.
 * All rights reserved.
 *
 * \file sigleton.hpp
@@ -16,8 +16,15 @@
 #include <string>
 #include <vector>
 #include <stdarg.h>
+#include "encode/strconv.h"
 
-std::string a;
+#ifndef _T
+#	ifdef UNICODE
+#		define _T(txt) L##txt
+#	else
+#		define _T(txt) txt
+#	endif
+#endif
 
 namespace daxia
 {
@@ -27,6 +34,7 @@ namespace daxia
 	public:
 		String_base();
 		String_base(const Elem* str);
+		String_base(const Elem* str, int count);
 		String_base(const std::basic_string<Elem, Traits, Alloc>& str);
 
 		// VC里CString的功能
@@ -40,17 +48,28 @@ namespace daxia
 		int Find(Elem ch, int start = 0) const;
 		int Find(const Elem* str, int start = 0) const;
 		void Format(const Elem* format, ...);
-		void FormatV(const Elem* format, va_list valist);
+		inline void FormatV(const Elem* format, va_list valist);
 		String_base Left(int count) const; 
 		String_base Right(int count) const;
 		String_base Mid(int start, int count) const;
 		String_base Tokenize(const Elem* sub, int& start) const;
 		int Replace(Elem oldch, Elem newch);
 		int Replace(const Elem* oldstr, const Elem* newstr);
+		String_base& MakeLower();
+		String_base& MakeUpper();
+		int Compare(const Elem* str) const;
+		int CompareNoCase(const Elem* str) const;
 		// 其他方法
 	public:
+		// 获取哈希值
+		std::size_t Hash() const;
 		// 分割字符串
 		void Split(const Elem* sub, std::vector<String_base>& strings) const;
+
+		// 编码转换 
+		inline String_base<wchar_t, std::char_traits<wchar_t>, std::allocator<wchar_t>> ToUnicode() const;
+		inline String_base<char, std::char_traits<char>, std::allocator<char>> ToAnsi() const;
+		inline String_base<char, std::char_traits<char>, std::allocator<char>> ToUtf8() const;
 	public:
 		String_base& operator=(Elem ch)
 		{
@@ -82,7 +101,12 @@ namespace daxia
 			return *this;
 		}
 
-		operator const Elem*()
+		bool operator==(const Elem* str) const
+		{
+			return Compare(str) == 0;
+		}
+
+		operator const Elem*() const
 		{
 			return v_.c_str();
 		}
@@ -101,19 +125,25 @@ namespace daxia
 	template<class Elem, class Traits, class Alloc>
 	daxia::String_base<Elem, Traits, Alloc>::String_base(const Elem* str)
 	{
-		v_ = std::basic_string<Elem, Traits, Alloc>(str);
+		v_.append(str);
+	}
+
+	template<class Elem, class Traits, class Alloc>
+	daxia::String_base<Elem, Traits, Alloc>::String_base(const Elem* str, int count)
+	{
+		v_.append(str, count);
 	}
 
 	template<class Elem, class Traits, class Alloc>
 	daxia::String_base<Elem, Traits, Alloc>::String_base(const std::basic_string<Elem, Traits, Alloc>& str)
 	{
-		v_ = std::basic_string<Elem, Traits, Alloc>(str);
+		v_.append(str, 0, -1);
 	}
 
 	template<class Elem, class Traits, class Alloc>
 	int daxia::String_base<Elem, Traits, Alloc>::GetLength() const
 	{
-		return v_.length();
+		return static_cast<int>(v_.length());
 	}
 
 	template<class Elem, class Traits, class Alloc>
@@ -127,10 +157,10 @@ namespace daxia
 	{
 		if (length == 0)
 		{
-			return const_cast<Elem*>GetString();
+			return const_cast<Elem*>(GetString());
 		}
 
-		int newLength = v_.size();
+		size_t newLength = v_.size();
 		if (newLength > 1024 * 1024 * 1024)
 		{
 			newLength += 1024 * 1024;
@@ -155,7 +185,7 @@ namespace daxia
 	{
 		if (maxCount == -1 || maxCount > static_cast<int>(v_.size()))
 		{
-			maxCount = v_.size();
+			maxCount = static_cast<int>(v_.size());
 		}
 
 		v_.resize(strlen(maxCount));
@@ -176,13 +206,13 @@ namespace daxia
 	template<class Elem, class Traits, class Alloc>
 	int daxia::String_base<Elem, Traits, Alloc>::Find(Elem ch, int start /*= 0*/) const
 	{
-		return v_.find(ch, start);
+		return static_cast<int>(v_.find(ch, start));
 	}
 
 	template<class Elem, class Traits, class Alloc>
 	int daxia::String_base<Elem, Traits, Alloc>::Find(const Elem* str, int start /*= 0*/) const
 	{
-		return v_.find(str, start);
+		return static_cast<int>(v_.find(str, start));
 	}
 
 	template<class Elem, class Traits, class Alloc>
@@ -194,21 +224,36 @@ namespace daxia
 		va_end(valist);
 	}
 
-	template<class Elem, class Traits, class Alloc>
-	void daxia::String_base<Elem, Traits, Alloc>::FormatV(const Elem* format, va_list valist)
+	template<>
+	void daxia::String_base<char, std::char_traits<char>, std::allocator<char>>::FormatV(const char* format, va_list valist)
 	{
-		Elem buffer[1024];
+		char buffer[1024];
 
 #ifdef _MSC_VER
 #	pragma warning(push)
 #	pragma warning(disable:4996)
 #endif // _MSC_VER
 
-#ifdef UNICODE
-		vswprintf(buffer, sizeof(buffer) / sizeof(Elem), format, valist);
-#else
-		vsnprintf(buffer, sizeof(buffer) / sizeof(Elem), format, valist);
-#endif
+		vsnprintf(buffer, sizeof(buffer) / sizeof(char), format, valist);
+
+#ifdef _MSC_VER
+#	pragma warning(pop)
+#endif // _MSC_VER
+
+		*this = buffer;
+	}
+
+	template<>
+	void daxia::String_base<wchar_t, std::char_traits<wchar_t>, std::allocator<wchar_t>>::FormatV(const wchar_t* format, va_list valist)
+	{
+		wchar_t buffer[1024];
+
+#ifdef _MSC_VER
+#	pragma warning(push)
+#	pragma warning(disable:4996)
+#endif // _MSC_VER
+
+		vswprintf(buffer, sizeof(buffer) / sizeof(wchar_t), format, valist);
 
 #ifdef _MSC_VER
 #	pragma warning(pop)
@@ -262,7 +307,7 @@ namespace daxia
 		else
 		{
 			int from = start;
-			start = v_.size() + 1;
+			start = static_cast<int>(v_.size()) + 1;
 			return Mid(from, -1);
 		}
 	}
@@ -316,6 +361,44 @@ namespace daxia
 	}
 
 	template<class Elem, class Traits, class Alloc>
+	String_base<Elem, Traits, Alloc>& daxia::String_base<Elem, Traits, Alloc>::MakeLower()
+	{
+		v_ = daxia::encode::Strconv::MakeLower(v_);
+		return *this;
+	}
+
+	template<class Elem, class Traits, class Alloc>
+	String_base<Elem, Traits, Alloc>& daxia::String_base<Elem, Traits, Alloc>::MakeUpper()
+	{
+		v_ = daxia::encode::Strconv::MakeUpper(v_);
+		return *this;
+	}
+
+	template<class Elem, class Traits, class Alloc>
+	int daxia::String_base<Elem, Traits, Alloc>::Compare(const Elem* str) const
+	{
+		return v_.compare(str);
+	}
+
+	template<class Elem, class Traits, class Alloc>
+	int daxia::String_base<Elem, Traits, Alloc>::CompareNoCase(const Elem* str) const
+	{
+		String_base<Elem, Traits, Alloc> temp1(v_);
+		String_base<Elem, Traits, Alloc> temp2(str);
+		temp1.MakeLower();
+		temp2.MakeLower();
+
+		return temp1.Compare(temp2);
+	}
+
+	template<class Elem, class Traits, class Alloc>
+	std::size_t daxia::String_base<Elem, Traits, Alloc>::Hash() const
+	{
+		return std::hash<std::basic_string<Elem, Traits, Alloc>>()(v_);
+	}
+
+
+	template<class Elem, class Traits, class Alloc>
 	void daxia::String_base<Elem, Traits, Alloc>::Split(const Elem* sub, std::vector<String_base>& strings) const
 	{
 		strings.clear();
@@ -328,6 +411,42 @@ namespace daxia
 				strings.push_back(str);
 			}
 		}
+	}
+
+	template<>
+	String_base<wchar_t, std::char_traits<wchar_t>, std::allocator<wchar_t>> daxia::String_base<wchar_t, std::char_traits<wchar_t>, std::allocator<wchar_t>>::ToUnicode() const
+	{
+		return *this;
+	}
+
+	template<>
+	String_base<wchar_t, std::char_traits<wchar_t>, std::allocator<wchar_t>> daxia::String_base<char, std::char_traits<char>, std::allocator<char>>::ToUnicode() const
+	{
+		return daxia::encode::Strconv::Ansi2Unicode(*this);
+	}
+
+	template<>
+	String_base<char, std::char_traits<char>, std::allocator<char>> daxia::String_base<char, std::char_traits<char>, std::allocator<char>>::ToAnsi() const
+	{
+		return *this;
+	}
+
+	template<>
+	String_base<char, std::char_traits<char>, std::allocator<char>> daxia::String_base<wchar_t, std::char_traits<wchar_t>, std::allocator<wchar_t>>::ToAnsi() const
+	{
+		return daxia::encode::Strconv::Unicode2Ansi(*this);
+	}
+
+	template<>
+	String_base<char, std::char_traits<char>, std::allocator<char>> daxia::String_base<char, std::char_traits<char>, std::allocator<char>>::ToUtf8() const
+	{
+		return daxia::encode::Strconv::Ansi2Utf8(*this);
+	}
+
+	template<>
+	String_base<char, std::char_traits<char>, std::allocator<char>> daxia::String_base<wchar_t, std::char_traits<wchar_t>, std::allocator<wchar_t>>::ToUtf8() const
+	{
+		return daxia::encode::Strconv::Unicode2Utf8(*this);
 	}
 
 	template<class Elem, class Traits, class Alloc>
