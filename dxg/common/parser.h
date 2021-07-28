4631866d3026b301c6ec15a749f3cf34d58b1dfb
zhouyu <zhouyu-jt@163.com>
2021-07-28 15:16:57 +0800

    消息头内存对其跨平台处理

--- dxg/common/parser.h
+++ dxg/common/parser.h
@@ -62,7 +62,15 @@ namespace daxia
 				DefaultParser(){}
 				~DefaultParser(){}
 			public:
+#ifdef _MSC_VER
+#define ATTRIBUTE_PACKED
+#else
+#define ATTRIBUTE_PACKED  __attribute__((packed))
+#endif // _MSC_VER
+
+#ifdef _MSC_VER
 #pragma pack(1)
+#endif
 				// 数据包头
 				struct PacketHead
 				{
@@ -70,8 +78,13 @@ namespace daxia
 					byte	hearbeat;		// 心跳包标识
 					unsigned int len;		// 数据长度，不包括本包头
 					unsigned long reserve;	// 保留数据
-				};
+				}ATTRIBUTE_PACKED;
+
+#ifdef _MSC_VER
 #pragma pack()
+#endif
+
+#undef ATTRIBUTE_PACKED
 			public:
 				virtual bool Marshal(daxia::dxg::common::BasicSession* session, 
 					const daxia::dxg::common::byte* data, 
