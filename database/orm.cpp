bd6f3f2c6eac92f2a59965ac5a9677b5e9f61bfc
zhouyu <zhouyu-jt@163.com>
2021-07-29 19:12:59 +0800

    大致完成Insert方法

--- database/orm.cpp
+++ database/orm.cpp
@@ -6,33 +6,67 @@ namespace daxia
 {
 	namespace database
 	{
-
-		bool Orm::Insert(const char* format, ...)
+		daxia::string Orm::Insert(const daxia::reflect::Reflect_base* table)
 		{
+			using namespace daxia::reflect;
+
 			daxia::string sql;
-			va_list vl;
-			va_start(vl, format);
-			sql.FormatV(format, vl);
-			va_end(vl);
+			sql.Format("insert into %s(", table->Tag(ORM));
 
-			size_t pos;
-			daxia::string test = sql.Tokenize(" ", pos);
-			if (test.CompareNoCase("insert") != 0) return false;
-
-			Insert(sql);
-		}
-
-		bool Orm::Insert(const Reflect_base* table)
-		{
-			daxia::string sql;
-			sql.Format("insert into %s(", table.Tag(ORM));
-
-			daxia::string temp;
-			auto layout = table.Layout();
+			daxia::string fieldList;
+			auto layout = table->Layout();
 			for (auto iter = layout.begin(); iter != layout.end(); ++iter)
 			{
-				Reflect_base* field = 
+				unsigned long offset = iter->second.get<unsigned long>(REFLECT_LAYOUT_FIELD_OFFSET, 0);
+
+				const Reflect_base* reflectBase = nullptr;
+				try{ reflectBase = dynamic_cast<const Reflect_base*>(reinterpret_cast<const Reflect_helper*>(reinterpret_cast<const char*>(table->ValueAddr()) + offset)); }
+				catch (const std::exception&){}
+				if (reflectBase == nullptr) continue;
+
+				std::string tag = reflectBase->Tag(ORM);
+				if (!tag.empty())
+				{
+					char ch = fieldList.IsEmpty() ? '(' : ',';
+					fieldList += ch;
+					fieldList += tag;
+				}
 			}
+			fieldList += ") values";
+
+			daxia::string valueList;
+			for (auto iter = layout.begin(); iter != layout.end(); ++iter)
+			{
+				unsigned long offset = iter->second.get<unsigned long>(REFLECT_LAYOUT_FIELD_OFFSET, 0);
+
+				const Reflect_base* reflectBase = nullptr;
+				try{ reflectBase = dynamic_cast<const Reflect_base*>(reinterpret_cast<const Reflect_helper*>(reinterpret_cast<const char*>(table->ValueAddr()) + offset)); }
+				catch (const std::exception&){}
+				if (reflectBase == nullptr) continue;
+
+				char ch = valueList.IsEmpty() ? '(' : ',';
+				valueList += ch;
+
+				//if ()
+				//{
+				//}
+				//
+				//	
+				//valueList += tag;
+			}
+			valueList += ')';
+
+			sql += fieldList + valueList;
+
+			auto recodset = command_->Excute(sql);
+			daxia::string err = recodset->GetLastError();
+
+			if (!err.IsEmpty())
+			{
+				//recodset->
+			}
+
+			return err;
 		}
 
 	}
--- database/orm.h
+++ database/orm.h
@@ -1,15 +1,44 @@
+#include <memory>
 #include "../string.hpp"
-#include "../reflect/reflect_base.h"
+#include "../reflect/reflect.hpp"
 
 namespace daxia
 {
 	namespace database
 	{
-		class Orm_base
+		class Recordset
+		{
+		public:
+			Recordset();
+			~Recordset();
+		public:
+			virtual bool Eof() = 0;
+			virtual bool Bof() = 0;
+			virtual void Next() = 0;
+			virtual daxia::string GetLastError() = 0;
+			virtual long long  ScopeIdentity() = 0;
+			template<class ValueType>
+			ValueType Get(const char* field)
+			{
+				ValueType v;
+				GetField(field,v);
+				return v;
+			}
+		protected:
+			virtual void GetField(const char* field, int& v) = 0;
+			virtual void GetField(const char* field, long long& v) = 0;
+			virtual void GetField(const char* field, float& v) = 0;
+			virtual void GetField(const char* field, double& v) = 0;
+			virtual void GetField(const char* field, daxia::string& v) = 0;
+		};
+
+		class Command
 		{
 		protected:
-			Orm_base() {}
-			virtual ~Orm_base{}
+			Command();
+			virtual ~Command();
+		public:
+			virtual std::shared_ptr<Recordset> Excute(const daxia::string& sql) = 0;
 		};
 
 		class Orm
@@ -25,19 +54,12 @@ namespace daxia
 			Orm(Driver driver, const daxia::string& connectString);
 			virtual ~Orm();
 		public:
-			virtual bool Insert(const daxia::string& sql) = 0;
-			virtual bool Delete(const daxia::string& sql) = 0;
-			virtual bool Query(const daxia::string& sql) = 0;
-			virtual bool Update(const daxia::string& sql) = 0;
-			virtual bool Excute(const daxia::string& sql) = 0;
-			bool Insert(const char* format, ...);
-			bool Insert(const Reflect_base* table);
-			bool Delete(const char* format, ...);
-			bool Delete(const Reflect_base* table);
-			bool Query(const char* format, ...);
-			bool Query(Reflect_base* table);
-			bool Update(const char* format, ...);
-			bool Update(Reflect_base* table);
+			daxia::string Insert(const daxia::reflect::Reflect_base* table);
+			daxia::string Delete(const daxia::reflect::Reflect_base* table);
+			daxia::string Query(daxia::reflect::Reflect_base* table);
+			daxia::string Update(daxia::reflect::Reflect_base* table);
+		private:
+			std::shared_ptr<Command> command_;
 		};
 	}
 }
