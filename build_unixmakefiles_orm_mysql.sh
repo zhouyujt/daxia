17ba1b6b82e9310a4c4e6cbf1f6869ef719f4c98
zhouyu <zhouyu-jt@163.com>
2021-08-12 12:06:46 +0800

    完善生成make脚本

--- build_unixmakefiles_orm_mysql.sh
+++ build_unixmakefiles_orm_mysql.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 cd build
 cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DORM_MYSQL=1 -DORM_SQLITE=0 ../
--- build_unixmakefiles_orm_sqlite.sh
+++ build_unixmakefiles_orm_sqlite.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 cd build
 cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DORM_MYSQL=0 -DORM_SQLITE=1 ..
