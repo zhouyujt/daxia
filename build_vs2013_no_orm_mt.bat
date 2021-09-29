ee9ecca097ec6eaaa4465adb0fa82a068a83711d
zhouyu <zhouyu-jt@163.com>
2021-09-29 12:00:04 +0800

    增加MT编译批处理

---
+++ build_vs2013_no_orm_mt.bat
@@ -0,0 +1,12 @@
+@echo off
+rd /S /Q build_mt >nul 2>nul
+mkdir build_mt
+cd build_mt
+
+cmake -G "Visual Studio 12 2013" -DORM_MYSQL=0 -DORM_SQLITE=0 -DMT=1 ../
+
+mkdir x64
+cd x64
+cmake -G "Visual Studio 12 2013 Win64" -DORM_MYSQL=0 -DORM_SQLITE=0  -DMT=1 ../../
+
+pause
