5fc823414096d7bfb17615c828a854ce4884c53b
zhouyu <zhouyu-jt@163.com>
2021-10-29 12:24:09 +0800

    修复单实例模板线程安全问题

--- singleton.hpp
+++ singleton.hpp
@@ -7,12 +7,14 @@
  * \author 漓江里的大虾
  * \date 十一月 2018
  * 
- * 提供单实例功能
+ * 单实例模板类
+ * 提供多线程安全的单实例功能
  *
  */
 
 #ifndef __DAXIA_SINGLETON_HPP
 #define __DAXIA_SINGLETON_HPP
+#include <mutex>
 
 namespace daxia
 {
@@ -28,10 +30,24 @@ namespace daxia
 	public:
 		static T& Instance()
 		{
-			static T v;
-			return v;
+			std::call_once(flag_, [&]()
+			{
+				static T v;
+				instance_ = &v;
+			});
+
+			return *instance_;
 		}
+	private:
+		static T* instance_;
+		static std::once_flag flag_;
 	};
+
+	template<class T>
+	T* Singleton<T>::instance_;
+
+	template<class T>
+	std::once_flag Singleton<T>::flag_;
 }
 
 #endif // !__DAXIA_SINGLETON_HPP
